fefadf091aa1c9d880fef68ee95ae1518a249dd7
Adnan Ali Lone <Adnan@mohsins-macbook-pro.local>
2015-12-31 00:17:37 -0600

    version 1

---
+++ finalTipApp/SettingsViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SettingsViewController.swift
+//  finalTipApp
+//
+//  Created by Adnan Ali Lone on 12/30/15.
+//  Copyright © 2015 Adnan Ali Lone. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+
+    @IBOutlet weak var percentagePicker: UIPickerView!
+    
+    let pickerData = ["18%","20%","22%"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        self.percentagePicker.dataSource = self
+        self.percentagePicker.delegate = self
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        print(row)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(row, forKey: "index")
+        defaults.synchronize()
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+        
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let index = defaults.integerForKey("index")
+        
+        percentagePicker.selectRow(index, inComponent: 0, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- finalTipApp/ViewController.swift
+++ finalTipApp/ViewController.swift
@@ -10,16 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //*********
+    
+    override func viewDidAppear(animated: Bool) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let index = defaults.integerForKey("index")
+        
+        var tipPercentages = [0.18, 0.20, 0.22]
+        tipControl.selectedSegmentIndex = index
+        
+        calculateTipAndTotal(tipPercentages[index])
+        
+        
+        
+    }
+    
+//**********
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.20, 0.22]
+        let percentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        calculateTipAndTotal(percentage)
+        
+      
+        
+        
+    }
+
+    //*****
+    func calculateTipAndTotal(percentage: Double){
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * percentage
+        let total = tip + billAmount
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%0.2f", tip)
+        totalLabel.text = String(format: "$%0.2f",total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+        
+    }
 }
 

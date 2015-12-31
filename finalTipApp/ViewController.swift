7f8946ded25fa9a36a51ce3694af5bfe77ecd0a7
Adnan Ali Lone <Adnan@mohsins-macbook-pro.local>
2015-12-30 19:21:40 -0600

    Initial Commit

---
+++ finalTipApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalTipApp
+//
+//  Created by Adnan Ali Lone on 12/30/15.
+//  Copyright © 2015 Adnan Ali Lone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
